b5699807bb676fe1171a6d6094640684b521dfe5
Michael <micheaol80@gmail.com>
2022-07-14 14:05:19 +0100

    Add logic to incript password

--- controllers/setPassword.js
+++ controllers/setPassword.js
@@ -1,4 +1,5 @@
 import AuthPassword from "../models/AuthPassword.js";
+import bcrypt from "bcryptjs";
 import User from "../models/User.js";
 
 
@@ -19,16 +20,38 @@ export const getPasswordRoutes = async(req, res) => {
 }
 
 export const setPasswordRoutes = async(req, res) => {
-    const { password, confirmPassword } = req.body;
+    let { password, confirmPassword } = req.body;
 
     const profileFields = {};
     profileFields.user = req.user.id
 
+
+    const salt = await bcrypt.genSalt(10);
+    password = await bcrypt.hash(password, salt);
+
+
     if (password) profileFields.password = password;
     if (confirmPassword) profileFields.confirmPassword = confirmPassword;
 
-    console.log(password)
-    console.log(confirmPassword)
+    try {
 
-    res.send("Hello")
+        let profile = await AuthPassword.findOne({ user: req.user.id });
+
+        if (profile) {
+            profile = await AuthPassword.findOneAndUpdate({ user: req.user.id }, { $set: profileFields }, { new: true });
+
+            return res.json(profile)
+        }
+
+        profile = new AuthPassword(profileFields);
+
+        await profile.save()
+
+        res.json(profile)
+
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send('Server error')
+    }
 }
