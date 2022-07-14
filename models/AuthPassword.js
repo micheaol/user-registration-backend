84adfb53c2ea02988006e74fa25e2d28c6256bc5
Michael <micheaol80@gmail.com>
2022-07-14 11:48:20 +0100

    Add user ref to the Auth password model

--- models/AuthPassword.js
+++ models/AuthPassword.js
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
 
 const AuthPasswordSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId
+    },
+
     password: {
         type: String,
         require: true
