5dd87a9095d408a598ed2998d40624f78cf90e62
Michael <micheaol80@gmail.com>
2022-07-14 07:47:46 +0100

    Set up app for the server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello")
+import express from "express";
+import bodyParser from 'body-parser';
+import mongoose from "mongoose";
+import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+
+app.use(cors());
