c0e2fc500554dfcc789ff2eb28280e62ddd24d8c
Michael <micheaol80@gmail.com>
2022-07-14 09:22:27 +0100

    Add users routes and controller

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 import express from "express";
 import bodyParser from 'body-parser';
-import mongoose from "mongoose";
 import cors from 'cors';
+
+
+import usersRoutes from './routes/user.js';
+import connectDB from "./config/db.js";
 
 const app = express();
 
+app.use('/api/v1/users', usersRoutes);
+
+
+const PORT = process.env.PORT || 5000;
+
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
-
 app.use(cors());
+
+
+app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`))
+connectDB();
